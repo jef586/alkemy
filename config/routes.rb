2c8dde5235eed072b2f1fb189037f7c448337648
MartinKarkovich <martin.karkovich@gmail.com>
2021-07-27 18:26:30 -0300

    created routes for Organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root "home#show"
+  namespace :api do
+    namespace :v1 do
+      resources :organizations
+      put 'organizations/:id/restore', to: 'organizations#restore'
+      patch 'organizations/:id/restore', to: 'organizations#restore'
+    end
+  end
 end
