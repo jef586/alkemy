92819d6c4b6bc3371883dea8a2c3decbf40606eb
camilogzlez <camilogzlez@gmail.com>
2021-07-27 19:00:38 -0300

    Categories model and controller done

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::RestaurantsController < Api::V1::BaseController
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
+end
---
+++ db/migrate/20210727192654_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :categories do |t|
+      t.string :name, null: false
+      t.text :description, null: true
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+    add_index :categories, :deleted_at
+  end
+end
