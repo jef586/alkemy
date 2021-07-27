028d1df4e23eb3f78b9c8bdd8c5e712c1cd18b9d
Gerardo Catalas <gerardo@catalasjuin.com>
2021-07-27 20:33:13 -0300

    Change phone column name from 'phone' to 'phone_number'

--- db/migrate/20210727230038_create_contacts.rb
+++ db/migrate/20210727230038_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration[6.1]
   def change
     create_table :contacts do |t|
       t.string :name
-      t.integer :phone
+      t.integer :phone_number
       t.text :message
 
       t.timestamps
