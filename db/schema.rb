440d88a9f3a7a9501fa146b40c020b6f82d61dce
Gerardo Catalas <gerardo@catalasjuin.com>
2021-07-27 20:35:34 -0300

    Change phone column name from 'phone' to 'phone_number'

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_230643) do
 
   create_table "contacts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.integer "phone"
+    t.integer "phone_number"
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
