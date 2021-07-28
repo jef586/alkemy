46ee84b7a69cbe0a20a1e2c7fbf2863a401a926b
MartinKarkovich <martin.karkovich@gmail.com>
2021-07-28 00:05:34 -0300

    create routing for Member

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :roles
       resources :categories
+      resources :members
+      put 'members/:id/restore', to: 'members#restore'
+      patch 'members/:id/restore', to: 'members#restore'      
     end
   end
 end
