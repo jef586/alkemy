05bfab82078ecb109046a3893ed0e8727b0163a6
MartinKarkovich <martin.karkovich@gmail.com>
2021-07-27 22:05:02 -0300

    empty methods

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -2,23 +2,18 @@ module Api
   module V1
     class RolesController < ApiController
       def index
-        render json: {message: 'roles_index'}
       end
       
       def show
-        render json: {message: 'roles_show'}
       end
 
       def create
-        render json: {message: 'roles_create'}
       end
 
       def update
-        render json: {message: 'roles_update'}
       end
 
       def destroy
-        render json: {message: 'roles_destroy'}
       end
     end
   end
