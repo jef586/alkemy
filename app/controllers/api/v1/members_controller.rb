d30cc01d66f31c10491520bac7c7fc018abc05cd
MartinKarkovich <martin.karkovich@gmail.com>
2021-07-28 00:10:27 -0300

    create controller for Member

---
+++ app/controllers/api/v1/members_controller.rb
@@ -0,0 +1,23 @@
+module Api 
+  module V1
+    class MembersController < ApiController
+      def index
+      end
+      
+      def show
+      end
+
+      def create
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+
+      def restore
+      end
+    end
+  end
+end
