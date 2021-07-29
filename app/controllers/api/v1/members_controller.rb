f0c4e0826b1e73925dfa8fafa5cabf35ea96eac3
MartinKarkovich <martin.karkovich@gmail.com>
2021-07-29 12:38:48 -0300

    run rubocop

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,9 +1,11 @@
-module Api 
+# frozen_string_literal: true
+
+module Api
   module V1
     class MembersController < ApiController
       def index
       end
-      
+
       def show
       end
 
