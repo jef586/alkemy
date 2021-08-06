d868105fba1e941b68d5f5d436dbb4db8388a14e
camilogzlez <camilogzlez@gmail.com>
2021-08-06 18:29:45 -0300

    Category deleted method without autorization done

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,20 +3,27 @@
 module Api
   module V1
     class CategoriesController < ApiController
+      before_action :set_category, only: %i[destroy]
       def index
       end
-
       def show
       end
-
       def update
       end
-
       def create
       end
-
       def destroy
+        if @category.present?
+          @category.destroy
+        else
+          render json: { errors: @category.errors.full_messages }, status: :unprocessable_entity
+        end
       end
+
+      private
+        def set_category
+          @category = Category.find(params[:id])
+        end
     end
   end
 end
