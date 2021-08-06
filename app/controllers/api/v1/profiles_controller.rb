4d8a4b1df7a1a716aec0b5ac0ebb2213a3d6f9aa
camilogzlez <camilogzlez@gmail.com>
2021-08-06 18:10:06 -0300

    ofensses corrected

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,12 +1,11 @@
-   # frozen_string_literal: true
+# frozen_string_literal: true
 
 module Api
-    module V1
-      class ProfilesController < ApiController
-        def show
-          render json: @current_user, serializer: Profiles::ProfileSerializer, status: :ok
-        end
+  module V1
+    class ProfilesController < ApiController
+      def show
+        render json: @current_user, serializer: Profiles::ProfileSerializer, status: :ok
       end
     end
+  end
 end
-
