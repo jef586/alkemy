1cc483fac64847f58ad66fd275875683b67cea4c
MartinKarkovich <martin.karkovich@gmail.com>
2021-08-06 04:12:08 -0300

    add destroy accion for user controller, run rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 
-class UsersController < ApplicationController
-  def index
-  end
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, only: [:show, :update, :destroy]
+      def index
+      end
 
-  def show
-  end
+      def show
+      end
 
-  def create
-  end
+      def create
+      end
 
-  def update
-  end
+      def update
+      end
 
-  def destroy
+      def destroy
+        @user.destroy
+      end
+
+      private
+        def set_user
+          @user = User.find(params[:id])
+        end
+    end
   end
 end
