528790e4234bf0364fe9aecd54d6f092a839fd24
Agus <agus.guitler@gmail.com>
2021-08-06 23:13:17 -0300

    Bug fixed

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -13,37 +13,12 @@ module Api
       end
 
       def create
-        # TO DO
-        # create_member = Member.new(creation_member_params)
-        if member.save
-          # TO DO
-          # render json: member, serializer: , status: :created
-        else
-          render json: { error: "We can't save your member" }, status: :unprocessable_entity
-        end
       end
 
       def update
-        if member.update(member_params)
-          # TO DO 
-          # render json: member, serializer , status: :ok
-        else
-          render json: { error: "We can't update the data" }, status: :unprocessable_entity
-        end
       end
 
       def destroy
-        member.destroy
-      end
-
-      private
-      
-      def member
-        @member || = Member.find(params[:id])
-      end
-
-      def member_params
-        params.require(:member).permit(:body)
       end
 
     end
