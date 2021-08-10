0217b0b070dea0fafac810bb823fb1af83be1e9f
MartinKarkovich <martin.karkovich@gmail.com>
2021-08-09 23:05:39 -0300

    temporally add method to S3Service class

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -1,9 +1,29 @@
 class S3Service < Aws::S3::Resource
   def list_buckets
+    bucket_list = []
     self.buckets.each do |bucket|
-      p bucket.name
+      bucket_list << bucket.name
     end
-    nil  
+    bucket_list
+  end
+
+  def upload_file_to_existent_bucket(bucket,folder,file_path)
+    if bucket_exists?(bucket)
+      key = "#{folder}/#{File.basename(file_path)}"
+      obj = self.bucket(bucket).object(key)
+      if obj.upload_file(file_path)
+        return "succesfully uploaded"
+      else 
+        return "cannot upload file"
+      end
+    else
+      return "bucket not found"
+    end    
+  end
+
+  # private
+  def bucket_exists?(bucket)
+    self.list_buckets.include? bucket  
   end
 
 end
