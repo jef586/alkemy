e88d7c7fff8592e77a16ba1cf3fd5ed51ea630cc
Agus <agus.guitler@gmail.com>
2021-08-10 17:00:40 -0300

    Bug fixes

--- app/serializers/api/v1/new_serializer.rb
+++ app/serializers/api/v1/new_serializer.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Api
-    module V1
-        class NewSerializer < ApplicationSerializer
-            attributes :id,
-                       :name,
-                       :content,
-                       :image_url
-        end
+  module V1
+    class NewSerializer < ApplicationSerializer
+      attributes :id,
+                 :name,
+                 :content,
+                 :image_url
     end
+  end
 end
