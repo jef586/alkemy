1b1ba4d098f5d11f2428bd269c2a1d37fdba1a8e
ezequiel <jeftikas@gmail.com>
2021-08-11 11:47:19 -0300

    endpoint-eliminacion-comentarios

--- app/controllers/api/v1/commentaries_controller.rb
+++ app/controllers/api/v1/commentaries_controller.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 
 module Api
-    module V1
-      class CommentariesController < ApiController
-        def index
-        end
-        def show
-        end
-        def update
-        end
-        def create
-        end
-        def destroy
-          if commentary.present?
-            commentary.destroy
-          end
-  
-          head :no_content
-        end
-  
-        private
-          def commentary
-            @commentary = Commentary.find(params[:id])
-          end
+  module V1
+    class CommentariesController < ApiController
+      def index
       end
+      def show
+      end
+      def update
+      end
+      def create
+      end
+      def destroy
+        if commentary.present?
+          commentary.destroy
+        end
+
+        head :no_content
+      end
+
+      private
+        def commentary
+          @commentary = Commentary.find(params[:id])
+        end
     end
   end
-  
+end
