ee24a2649f6deba36a682a885df33e148e08056b
Gerardo Catalas <gerardo@catalasjuin.com>
2021-08-12 20:53:16 -0300

    fix bug with create method

--- app/controllers/api/v1/commentaries_controller.rb
+++ app/controllers/api/v1/commentaries_controller.rb
@@ -18,7 +18,7 @@ module Api
       end
 
       def create
-        create_commentary = current_user.commentaries.new(create_commentary_params)
+        create_commentary = @current_user.commentaries.new(create_commentary_params)
         create_commentary.save
 
         if create_commentary.persisted?
