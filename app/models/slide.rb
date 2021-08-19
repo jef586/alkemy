c8cf0b60191ed423eb3b29c85eaad7d64773c3ee
MartinKarkovich <martin.karkovich@gmail.com>
2021-08-19 18:16:24 -0300

    add some instance methods inside slide.rb, add callback hook

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -5,10 +5,37 @@ class Slide < ApplicationRecord
   has_one_attached :image
   # Associations
   belongs_to :organization
+  # Callbacks
+  before_create :before_create_adjustment
   # Validations
   ## TODO add proper image validations
   validates :text,
             presence: true
 
-  validates :order, numericality: { only_integer: true }, allow_nil: true
+  validates :order,
+            numericality: { only_integer: true, greater_than_or_equal_to: 1 },
+            allow_nil: true
+  # Scopes
+  scope :where_order_greater_equal_than, -> (order) { where("slides.order >= ?", order) }
+  # Methods
+  def set_default_order
+    self.order = self.class.count + 1
+  end
+  def before_create_adjustment
+    return unless self.errors.empty?
+    if self.order.nil? || self.order > self.class.count
+      self.set_default_order
+    else # In case order is already taken.
+      self.adjust_order
+    end
+  end
+
+  private
+    def adjust_order
+      self.class.where_order_greater_equal_than(self.order)
+                .each do |slide|
+        slide.order += 1
+        slide.save(validate: false)
+      end
+    end
 end
