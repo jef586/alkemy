0f99df1d82e146466e6e5e2931df6796cfb86fba
camilogzlez <camilogzlez@gmail.com>
2021-08-21 12:35:50 -0300

    to save changes before pull

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
-describe "GET /admin/users" do
+RSpec.describe 'Users', type: :request do
+  describe 'POST /register' do
+    it 'authenticates the user' do
+      post '/auth/register', params: { user: { first_name: 'user1', last_name: 'last_name', 
+      email: 'email@email.com',
+      password: 'password'} }
 
-  it "returns all users" do
+      expect(response).to have_http_status(:created)
+      expect(json).to eq({
+                           'id' => User.last.id,
+                           'first_name' => 'user1',
+                           'token' => AuthenticationTokenService.call(User.last.id)
+                         })
+    end
   end
-
-  
 end
