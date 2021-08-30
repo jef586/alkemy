15a1bb9ba76ff15fe76282c2af47fccf6ece6987
Federico <fedeosov@gmail.com>
2021-08-30 18:47:41 -0300

    Member spec and order integrations

--- spec/requests/admin/members/show_spec.rb
+++ spec/requests/admin/members/show_spec.rb
@@ -2,16 +2,16 @@
 
 require "rails_helper"
 
-describe "GET /admin/posts/:id", type: :request do
-  let(:endpoint) { get admin_post_path(post) }
-  let(:post) { create(:post) }
+describe "GET /admin/members/:id", type: :request do
+  let(:endpoint) { get admin_member_path(member) }
+  let(:member) { create(:member) }
 
   context "when user is an admin" do
     let(:admin) { create(:user) }
 
     before { sign_in(admin) }
 
-    context "and the post exists" do
+    context "and the member exists" do
       before do
         endpoint
       end
@@ -20,25 +20,22 @@ describe "GET /admin/posts/:id", type: :request do
         expect(response).to have_http_status(200)
       end
       it "returns the correct id" do
-        expect(data["id"]).to eq(post.id)
+        expect(data["id"]).to eq(member.id)
       end
       it "returns the correct name" do
-        expect(data["name"]).to eq(post.name)
-      end
-      it "returns the correct content" do
-        expect(data["content"]).to eq(post.content)
+        expect(data["name"]).to eq(member.name)
       end
     end
 
-    context "and the post doesn't exists" do
-      let(:endpoint) { get admin_post_path(240) }
+    context "and the member doesn't exists" do
+      let(:endpoint) { get admin_member_path(240) }
 
       before { endpoint }
       it "has an HTTP status 404" do
         expect(response).to have_http_status(404)
       end
-      it "returns error message: 'Couldn't find Post...'" do
-        expect(body_json["error"]).to match(/Couldn't find Post/)
+      it "returns error message: 'Couldn't find Meber...'" do
+        expect(body_json["error"]).to match(/Couldn't find Member/)
       end
     end
   end
